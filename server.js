344cbc4e4b75e703c08918cbe25c85f0d3d075c2
Luiz Keese Tabacow <keese.luiz@gmail.com>
2018-03-21 17:43:42 -0300

    Created projects page

--- server.js
+++ server.js
@@ -42,6 +42,12 @@ app.get('/', (request, response) => {
     });
 });
 
+app.get('/projects', (request, response) => {
+    response.render('projects.hbs', {
+        pageTitle: 'Projects page'
+    });
+});
+
 app.get('/bad', (request, response) => {
     response.send({
         errorMessage: 'Unable to fetch page.',
